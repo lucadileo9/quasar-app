5571b2c02ce640da5ecbcf4b9cb94d27154c253f
lucadileo9 <lucadileo70@gmail.com>
2025-05-19 15:02:29 +0200

    style: npm run format

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-img
-      src="background.jpg"
-      class="fit absolute"
-    />
+    <q-img src="background.jpg" class="fit absolute" />
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
